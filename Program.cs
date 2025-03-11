0b0b33d30e93348348df64ccf6e6a72966493ed6
Yurii <y.dmytrenko11@gmail.com>
2025-03-11 18:03:05 +0200

    Add code

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main()
     {
-        
+        Array2D my_arr = new Array2D(2, 2);
+        my_arr[0, 1] = 10;
+        my_arr[0, 0] = 3;
+        my_arr[0, 1] = 8;
+        my_arr[1, 0] = 5;
+        my_arr[1, 1] = 9;
+        Console.WriteLine(my_arr.SumOfElements);
+        Console.WriteLine(my_arr[1]);
+
     }
 }
